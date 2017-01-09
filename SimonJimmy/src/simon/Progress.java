b4f0569d3b031ab2de2ff6306c520c6e81e22ec9
JaviyWang <jwang1899@bths.edu>
2017-01-09 18:44:48 -0500

    Update Progress.java
    Javiy is done

--- SimonJimmy/src/simon/Progress.java
+++ SimonJimmy/src/simon/Progress.java
@@ -9,7 +9,7 @@ import guiPractice.components.Component;
 
 public class Progress extends Component implements ProgressInterfaceJimmy {
 
-	private static final int WIDTH = 120;
+	private static final int WIDTH = 250;
 	private static final int HEIGHT = 50;
 
 	private boolean gameOver;
@@ -23,12 +23,12 @@ public class Progress extends Component implements ProgressInterfaceJimmy {
 
 
 	public void setRound(int roundNumber) {
-		round = "Round "+roundNumber;
+		round = "THE ROUND IS AT "+roundNumber;
 		update();
 	}
 
 	public void setSequenceSize(int size) {
-		sequence = "Sequence length "+size;
+		sequencelength = "THE SEQUENCE LENGTH IS "+size;
 		update();
 	}
 
@@ -44,7 +44,7 @@ public class Progress extends Component implements ProgressInterfaceJimmy {
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		FontMetrics fm = g.getFontMetrics();
 		if(gameOver){
-			g.setColor(new Color(255,55,90));
+			g.setColor(Color.black);
 			g.fillRect(0, 0, WIDTH, HEIGHT);
 			g.setColor(Color.white);
 			String go = "GAME OVER!";
@@ -52,7 +52,7 @@ public class Progress extends Component implements ProgressInterfaceJimmy {
 			g.drawString(sequence, (WIDTH - fm.stringWidth(sequence))/2, 40);
 
 		}else{
-			g.setColor(new Color(220,255,230));
+			g.setColor(Color.white);
 			g.fillRect(0, 0, WIDTH, HEIGHT);
 			g.setColor(Color.black);
 			g.drawRect(0, 0, WIDTH-1, HEIGHT-1);
