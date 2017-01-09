9027a141770c0dcd88d58d5bf00d851e3b532578
Jimmy <Jimmy@Jimmy-HP.home>
2017-01-08 21:52:33 -0500

    DONE WITH MOVE STARTED ON BUTTON AND PROGRESS

---
+++ SimonJimmy/src/simon/Progress.java
@@ -0,0 +1,32 @@
+package simon;
+
+import java.awt.Graphics2D;
+
+import guiPractice.components.Component;
+
+public class Progress extends Component implements ProgressInterfaceJimmy {
+
+	public Progress(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setRound(int roundNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSequenceSize(int size) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Graphics2D g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
